ef85e0aaaaedb1b7968bae12c9eddb1c4e4a3e04
Yuan-ueng <yuan.yueh@northeastern.edu>
2025-06-24 17:29:50 -0700

    add login signup

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Vortex } from "@/components/ui/vortex";
+import { SignIn } from '@clerk/nextjs'
+
+export default function Page() {
+    return (
+        <div className="w-full] mx-auto rounded-md  h-screen overflow-hidden">
+            <Vortex
+                backgroundColor="black"
+                rangeY={800}
+                particleCount={500}
+                baseHue={120}
+                className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
+            >
+                <SignIn />
+            </Vortex>
+        </div>
+    );
+}
+
