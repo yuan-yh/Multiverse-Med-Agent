833adb3b859ba4ad92e5e76c4b07040672fb8443
Yuan-ueng <yuan.yueh@northeastern.edu>
2025-06-26 11:02:16 -0700

    update dashboard chat history

--- app/(routes)/dashboard/_components/HistoryList.tsx
+++ app/(routes)/dashboard/_components/HistoryList.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useState } from 'react'
 import Image from 'next/image'
+import { Button } from '@/components/ui/button';
 
 function HistoryList() {
     const [historyList, setHistoryList] = useState([]);
@@ -14,6 +15,7 @@ function HistoryList() {
                         <Image src={'/medieval-plague-doctor.jpg'} alt='consult-doctor' width={150} height={150} />
                         <h2 className='font-bold text-xl mt-5'>No Recent Consultations</h2>
                         <p>It looks like you haven't talked with any doctors yet.</p>
+                        <Button className='mt-3'>+ Start a Call</Button>
                     </div>
                     :
                     <div>List</div>
---
+++ app/(routes)/dashboard/_components/MedicalAgentList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MedicalAgentList() {
+    return (
+        <div>MedicalAgentList</div>
+    )
+}
+
+export default MedicalAgentList
