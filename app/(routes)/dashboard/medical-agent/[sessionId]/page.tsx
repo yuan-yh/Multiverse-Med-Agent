701e472186b3f524633c64c838f313a1e47bdbfa
Yuan-ueng <yuan.yueh@northeastern.edu>
2025-07-09 17:16:07 -0700

    add vapi stt agent

--- app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
+++ app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 
 import axios from 'axios';
+import 'dotenv/config';
+import Vapi from '@vapi-ai/web';
 import { useParams } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 import { medicalAgent } from '@/app/(routes)/dashboard/_components/MedicalAgentCard';
-import { Circle, PhoneCall } from 'lucide-react';
+import { Circle, PhoneCall, PhoneOff } from 'lucide-react';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 
@@ -20,6 +22,9 @@ type sessionDetail = {
 function MedicalVoiceAgent() {
     const { sessionId } = useParams();
     const [sessionDetail, setSessionDetail] = useState<sessionDetail>();
+    const [callStarted, setCallStarted] = useState(false);
+
+    const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_API_KEY!);
 
     useEffect(() => {
         sessionId && GetSessionDetails();
@@ -31,10 +36,29 @@ function MedicalVoiceAgent() {
         setSessionDetail(result.data);
     }
 
+    const startCall = () => {
+        // Start voice conversation
+        vapi.start(process.env.NEXT_PUBLIC_VAPI_VOICE_ASSISTANT_ID);
+        // Listen for events
+        vapi.on('call-start', () => {
+            console.log('Call started');
+            setCallStarted(true);
+        });
+        vapi.on('call-end', () => {
+            console.log('Call ended');
+            setCallStarted(false);
+        });
+        vapi.on('message', (message) => {
+            if (message.type === 'transcript') {
+                console.log(`${message.role}: ${message.transcript}`);
+            }
+        });
+    }
+
     return (
-        <div className='p-5 border rounded-3xl text-secondary'>
+        <div className='p-5 border rounded-3xl bg-secondary'>
             <div className='flex justify-between items-center'>
-                <h2 className='p-1 px-2 border rounded-md flex gap-2 items-center'><Circle className='h-4 w-4' /> Not Connected ...</h2>
+                <h2 className='p-1 px-2 border rounded-md flex gap-2 items-center'><Circle className={`h-4 w-4 rounded-full ${callStarted ? 'bg-green-500' : 'bg-red-500'}`} /> {callStarted ? 'Connected' : 'Not Connected ...'}</h2>
                 <h2 className='font-bold text-xl text-gray-400'>00:00</h2>
             </div>
             {sessionDetail && <div className='flex items-center flex-col mt-10'>
@@ -48,7 +72,12 @@ function MedicalVoiceAgent() {
                     <h2 className='text-lg'>User Msg</h2>
                 </div>
 
-                <Button className='mt-20'><PhoneCall />Start Call</Button>
+                {!callStarted
+                    ?
+                    <Button className='mt-20' onClick={startCall}><PhoneCall />Start Call</Button>
+                    :
+                    <Button className='mt-20' variant={'destructive'}><PhoneOff />End Call</Button>
+                }
             </div>}
         </div>
     )
