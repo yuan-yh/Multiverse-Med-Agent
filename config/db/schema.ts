f036e78d4e0358307d3ef38fad907dea9107d8d2
Yuan-ueng <yuan.yueh@northeastern.edu>
2025-07-17 15:35:20 -0700

    fix db schema definition

--- config/db/schema.ts
+++ config/db/schema.ts
@@ -1,5 +1,6 @@
 import { json, text } from "drizzle-orm/gel-core";
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+// import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, serial } from 'drizzle-orm/pg-core';
 
 export const usersTable = pgTable("users", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -8,13 +9,25 @@ export const usersTable = pgTable("users", {
     credits: integer(),
 });
 
-export const sessionChatTable = pgTable("sessionChatTable", {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    sessionId: varchar().notNull(),
-    notes: text(),
-    selectedDoctor: json(),
-    conversation: json(),
-    report: json(),
-    createdBy: varchar().references(() => usersTable.email),
-    createdOn: varchar(),
-});
+// export const sessionChatTable = pgTable('sessionChatTable', {
+//     id: integer().primaryKey().generatedAlwaysAsIdentity(),
+//     sessionId: varchar().notNull(),
+//     notes: text(),
+//     selectedDoctor: json(),
+//     conversation: json(),
+//     report: json(),
+//     createdBy: varchar().references(() => usersTable.email),
+//     createdOn: varchar(),
+// });
+
+export const sessionChatTable = pgTable("sessionChatTable", (t) => ({
+    id: t.serial("id").primaryKey(),
+    sessionId: t.varchar("sessionId").notNull(),
+    notes: t.text("notes"),
+    selectedDoctor: t.json("selectedDoctor"),
+    conversation: t.json("conversation"),
+    report: t.json("report"),
+    createdBy: t.varchar("createdBy").references(() => usersTable.email),
+    createdOn: t.varchar("createdOn"),
+}));
+
